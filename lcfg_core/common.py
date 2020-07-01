2de6e3d694b6fc83af6c189bb22cf760f21781ba
Stephen Quinney <stephen@jadevine.org.uk>
2020-07-01 20:22:52 +0100

    Module for common classes and enums
    This provides various classes and enums which are commonly needed
    across the whole project.

---
+++ lcfg_core/common.py
@@ -0,0 +1,39 @@
+from enum import Enum, IntFlag
+
+class LCFGStatus(IntFlag):
+    ERROR = 0
+    WARN  = 1
+    OK    = 2
+
+class LCFGChange(IntFlag):
+    ERROR    =   0
+    NONE     =   1
+    MODIFIED =   2
+    ADDED    =   4
+    REPLACED =   8
+    REMOVED  =  16
+
+class LCFGOption(IntFlag):
+    NONE           =    0
+    NOCONTEXT      =    1
+    NOPREFIX       =    2
+    NEWLINE        =    4
+    NOVALUE        =    8
+    NOTEMPLATES    =   16
+    ALLOW_NOEXIST  =   32
+    ENCODE         =   64
+    ALL_CONTEXTS   =  128
+    ALL_PRIORITIES =  256
+    USE_META       =  512
+    ALL_VALUES     = 1024
+    COMPAT         = 2048
+    LEGACY         = 4096
+    NEW            = 8192
+
+class LCFGMergeRule(IntFlag):
+    NONE             =  0
+    KEEP_ALL         =  1
+    SQUASH_IDENTICAL =  2
+    USE_PRIORITY     =  4
+    USE_PREFIX       =  8
+    REPLACE          = 16
