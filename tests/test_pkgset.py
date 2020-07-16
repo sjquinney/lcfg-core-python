995216282722ddf77321c47be9d96278855abe6f
Stephen Quinney <stephen@jadevine.org.uk>
2020-07-16 06:20:34 +0100

    Actually set LCFGPackageSet...
    oopps, it was testing LCFGPackageList rather than LCFGPackageSet

--- tests/test_pkgset.py
+++ tests/test_pkgset.py
@@ -1,9 +1,9 @@
 import pytest
 
-from lcfg_core.packages import LCFGPackageList
+from lcfg_core.packages import LCFGPackageSet
 
-def test_pkglist_new():
-    l1 = LCFGPackageList()
+def test_pkgset_new():
+    l1 = LCFGPackageSet()
     assert l1.size == 0
     assert len(l1) == 0
     assert not l1
